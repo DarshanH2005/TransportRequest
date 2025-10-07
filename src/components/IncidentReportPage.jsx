cfe2fc19f721d360918e03cdd362e2d06ef54ae3
Darshan H <darshan1970h@gmail.com>
2025-10-07 16:50:22 +0530

    asdf

--- src/components/IncidentReportPage.jsx
+++ src/components/IncidentReportPage.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Avatars from './Avatars';
-import RequiredInfo from './RequiredInfo';
-import ViolationHistory from './ViolationHistory';
-import OptionalApprovers from './OptionalApprovers';
 
 const TransportRequestPage = () => {
   return (
--- src/components/VisaFormSection.jsx
+++ src/components/VisaFormSection.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import '../styles/RequiredInfo.css';
 import '../styles/formControls.css';
 import ApplyHeading from './ApplyHeading';
-import EmployeeNameInput from './EmployeeNameInput';
-import EmployeeDetailsCard from './EmployeeDetailsCard';
 
 const VisaFormSection = () => {
   return (
