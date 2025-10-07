95a4da7ec4562ddd12adc47eca3acf58b70eca81
Darshan H <darshan1970h@gmail.com>
2025-10-07 21:28:52 +0530

    asdf

--- src/components/ApplyHeading.jsx
+++ src/components/ApplyHeading.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './ApplyHeading.module.css';
+import styles from '../styles/ApplyHeading.module.css';
 
 const ApplyHeading = ({ className = '', onClick }) => {
   return (
